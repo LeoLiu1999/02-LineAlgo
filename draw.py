7367843dde6beed754a604951669aafd8c7c609e
TakingTheL <yliu6@stuy.edu>
2018-02-08 21:16:12 -0500

    progress

---
+++ draw.py
@@ -0,0 +1,71 @@
+from display import *
+
+def draw_line( x0, y0, x1, y1, screen, color ):
+    if(x0 > x1):
+        draw_line(x1, y1, x0, y0, screen, color)
+        return
+    
+    #define constants
+    a = y1 - y0 #delta y
+    b = x0 - x1 #- delta x
+    x = x0
+    y = y0
+    
+    #if octant 1
+    if ((-1 * b) > a and a > 0):
+        d = a + a + b
+
+        a += a #a => 2a
+        b += b #b => 2b
+        while(x <= x1):
+            plot(screen, color, x, y)
+            if (d > 0):
+                y ++
+                d += b
+            x ++
+            d += a
+        return
+    
+    #if octant 2
+    if (a > (-1 * b)):
+        d = a + b + b
+
+        a += a #a => 2a
+        b += b #b => 2b
+        while(y <= y1):
+            plot(screen, color, x, y)
+            if (d < 0)
+                x ++
+                d += a
+            y ++
+            d += b
+        return
+    
+    #if octant 7
+    if (a < b):
+        d = a - b - b
+        a += a
+        b += b
+        while(x <= x1):
+            plot(screen, color, x, y)
+            if (d > 0):
+                x ++
+                d += a
+            y --
+            d += 2b
+        return
+    
+    #if octant 8
+    if (b < a and a < 0):
+        d = a + a - b
+        a += a
+        b += b
+        while(x <= x1):
+            plot(screen, color, x, y)
+            if ( d > 0):
+                y --
+                d -= b
+            x ++
+            d += a
+
+,m
