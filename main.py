e4e0aefee7e0ff6d64d42327967b3341ed7dd4d8
TakingTheL <yliu6@stuy.edu>
2018-02-11 17:39:34 -0500

    done

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0, 250, 500, 0, screen, color)
-draw_line(0, 250, 500, 500, screen, color)
-draw_line(250, 0, 500, 500, screen, color)
-draw_line(250, 0, 0, 500, screen, color)
-draw_line(500, 250, 0, 0 , screen, color)
-draw_line(500, 250, 0, 500, screen, color)
-draw_line(250, 500, 0, 0, screen, color)
-draw_line(250, 500, 500, 0, screen, color)
+for x in xrange(5):
+    draw_line(0, x * 100, 500, 0, screen, color)
+    draw_line(0, x * 100, 500, 500, screen, color)
+    draw_line(x * 100, 0, 500, 500, screen, color)
+    draw_line(x * 100, 0, 0, 500, screen, color)
+    draw_line(500, x * 100, 0, 0 , screen, color)
+    draw_line(500, x * 100, 0, 500, screen, color)
+    draw_line(x * 100, 500, 0, 0, screen, color)
+    draw_line(x * 100, 500, 500, 0, screen, color)
 
-#vertical/horizontal lines
-draw_line(250, 0, 250, 500, screen, color)
-draw_line(0, 250, 500, 250, screen, color)
+    #vertical/horizontal lines
+    draw_line(x * 100, 0, x * 100, 500, screen, color)
+    draw_line(0, x * 100, 500, x * 100, screen, color)
 
-#m=1/m=-1 lines
-draw_line(0, 0, 500, 500, screen, color)
-draw_line(0, 500, 500, 0, screen, color)
+    #m=1/m=-1 lines are already included
 
 display(screen)
 save_extension(screen, 'img.png')
